2ea6dd4694f6b8d11f64282493915f6181de716d
HADJIMI1 <salimhadjimi10@gmail.com>
2026-03-23 15:29:20 +0100

    prepare les modefication

---
+++ cypress/e2e/tests/verify-InformationProduct-Cart.cy.js
@@ -0,0 +1,24 @@
+import HomePage from "../../../pom/homePage"
+import InfoCartProduct from "../../../pom/infoCartProduct"
+import ActionProductToCart from "../../../action/addProdectToCart"
+
+describe("verifie cordonne", ()=>{
+let homePages=new HomePage()
+let infoCart=new InfoCartProduct()
+let actionButton=new ActionProductToCart()
+
+
+    it("verifie cordonne", ()=>{
+        homePages.gotoHomePage()
+        infoCart.selectProduct_Madame()
+        infoCart.productRS1000()
+        actionButton.closePopup()
+        infoCart.productRs15000()
+        actionButton.closePopup()
+        infoCart.viewProduct()
+        infoCart.priceProductRs15000()
+        infoCart.verifyPrice()
+        infoCart.descripProduct()
+        
+    })
+})
