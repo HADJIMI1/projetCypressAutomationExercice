4beecd86d49a062907fbe152c933bda5e3927570
HADJIMI1 <salimhadjimi10@gmail.com>
2026-03-23 18:41:57 +0100

    le nouveau commit pour test jenkies

--- cypress/e2e/tests/signupLogin.cy.js
+++ cypress/e2e/tests/signupLogin.cy.js
@@ -6,6 +6,7 @@ import fixtures from "../../fixtures/users.json"
 describe("page Signup and Login", ()=>{
 let signupLogin=new SignupLogin()
     // on doit executer ca en premier avant tout le reste obligatoire 
+    // je voir que tu veux faire une nouvelle modification toujour 
   beforeEach(() => {
     cy.visit("https://automationexercise.com/login")
   })
